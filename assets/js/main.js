b1efa0e3fb3645fb337030198faf6deb0dd17170
Jorge Madson <filholouco@gmail.com>
2018-11-07 01:37:19 -0200

    refactoring event functions for DRY

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,29 +5,25 @@
     var $emailInput = document.querySelector('[data-js="email"]');
     var $passInput = document.querySelector('[data-js="password"]');
     var $passconfirmInput = document.querySelector('[data-js="password-confirm"]');
-    
+
     //Events
     $passInput.addEventListener('input', passStrength, false);
     $nameInput.addEventListener('input', nameFilled, false);
     $emailInput.addEventListener('input', validateEmail, false);
     $passconfirmInput.addEventListener('input', confirmPass, false);
-    
+
     //Functions
     function passStrength(e) {
-        if (isPassStrength(e.target.value))
-            return e.target.style.borderColor = correctInputColor();
+        return e.target.style.borderColor = correctInputColor(isPassStrength(e.target.value));
     }
     function nameFilled(e) {
-        return e.target.style.borderColor = correctInputColor();
+        return e.target.style.borderColor = correctInputColor(true);
     }
     function validateEmail(e) {
-        if (isValidEmail(e.target.value))
-            return e.target.style.borderColor = correctInputColor();
+        return e.target.style.borderColor = correctInputColor(isValidEmail(e.target.value));
     }
     function confirmPass(e) {
-        if (e.target.value === $passInput.value)
-            return e.target.style.borderColor = correctInputColor();
-        return e.target.style.borderColor = '';
+        return e.target.style.borderColor = correctInputColor(e.target.value === $passInput.value);
     }
     function isPassStrength(pass) {
         return /^.{6,}$/.test(pass) && /\w/.test(pass) && /\d/.test(pass);
@@ -35,8 +31,10 @@
     function isValidEmail(email) {
         return /^[\w+.]+@\w+\.\w{2,}(?:\.\w{2})?$/m.test(email);
     }
-    function correctInputColor() {
-        return "#17D499";
+    function correctInputColor(valid) {
+        if (valid)
+            return '#17D499';
+        return '';
     }
 }
 )();
